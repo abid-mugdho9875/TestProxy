556b42b09af0f4501cb3904aa4c5ff2b70f1178f
abid <abidmugdho@gmail.com>
2023-11-16 12:32:14 +0600

    working for maximum node

--- models/ProxyModel.js
+++ models/ProxyModel.js
@@ -2,38 +2,42 @@ const axios = require('axios');
 
 class ProxyModel {
   static async fetchData(params) {
-    const {url,...param} = params
+    const {url,header1, header2, ...param} = params
     console.log(param)
     const options = {
       method: 'GET',
       url:url,
-      params: {
+      param,
+      // params: {
         
-        function: param.function,
-        interval:param.interval,
-        symbol:param.symbol,
-        referenceCurrencyUuid:param.referenceCurrencyUuid,
-        timePeriod:param.timePeriod,
-        tier:param.tier,
-        orderBy:param.orderBy,
-        orderDirection:param.orderDirection,
-        limit:param.limit,
-        offset:param.offset,
-        datatype: param.datatype,
-        outputsize: param.outputsize,
-        from_symbol:param.from_symbol,
-        to_symbol:param.to_symbol,
-        market:param.market,
-        time_period:param.timePeriod,
-        series_type:param.series_type,
-        format:param.format,
-        from:param.from,
-        to:param.to,
-        amount:param.amount
-      },
+      //   function: param.function,
+      //   interval:param.interval,
+      //   symbol:param.symbol,
+      //   referenceCurrencyUuid:param.referenceCurrencyUuid,
+      //   timePeriod:param.timePeriod,
+      //   tier:param.tier,
+      //   orderBy:param.orderBy,
+      //   orderDirection:param.orderDirection,
+      //   limit:param.limit,
+      //   offset:param.offset,
+      //   datatype: param.datatype,
+      //   outputsize: param.outputsize,
+      //   from_symbol:param.from_symbol,
+      //   to_symbol:param.to_symbol,
+      //   market:param.market,
+      //   time_period:param.timePeriod,
+      //   series_type:param.series_type,
+      //   format:param.format,
+      //   from:param.from,
+      //   to:param.to,
+      //   amount:param.amount,
+      //   tabname:param.tabname,
+      //   fromCurrency:param.fromCurrency,
+      //   to
+      // },
       headers: {
-        'X-RapidAPI-Key': '03f8490efcmsh81fdb3742186abep123901jsn758ea15cb04a',
-        'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+        'X-RapidAPI-Key': params.header1,
+        'X-RapidAPI-Host': params.header2,
       },
     };
 
